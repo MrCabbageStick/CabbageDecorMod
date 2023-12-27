ce187ee599d1222de3642e0eabed9e8b549d3686
MrCabbageStick <mrcabbagestick@gmail.com>
2023-12-27 21:37:56 +0100

    Initial commit

---
+++ src/client/java/mrcabbagestick/cabbagedecor/CabbageDecorClient.java
@@ -0,0 +1,10 @@
+package mrcabbagestick.cabbagedecor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CabbageDecorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
